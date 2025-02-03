9db32b693399dfea7d3e226ba62ecad37a0da885
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-02-02 22:46:15 -0800

    first commit

--- lib/view/screen/auth/sign_in.dart
+++ lib/view/screen/auth/sign_in.dart
@@ -5,6 +5,10 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign In"),
+      ),
+    );
   }
 }
