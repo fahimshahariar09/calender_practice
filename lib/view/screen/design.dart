78a45e8bdc54e7cba9c3936892e86b3cb60e68e0
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-02-05 21:50:14 -0800

    first commit

--- lib/view/screen/design.dart
+++ lib/view/screen/design.dart
@@ -6,19 +6,21 @@ class Design extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(
-                begin: Alignment.bottomRight,
-                stops: [0.1, 0.9],
-                colors: [Colors.green, Colors.orange.withBlue(3)])),
-        child: Column(
-          children: [
-
-          ],
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  stops: [0.1, 0.9],
+                  colors: [Colors.green, Colors.orange.withBlue(3)])),
+          child: Column(
+            children: [
+              Text("data"),
+            ],
+          ),
         ),
       ),
     );
