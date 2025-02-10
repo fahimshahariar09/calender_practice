a057ccac17e0afd3ccf49c7bc15a369074136623
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-02-09 20:57:28 -0800

    first commit

--- lib/view/screen/tabbar_screen/tabbar.dart
+++ lib/view/screen/tabbar_screen/tabbar.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+
 class Tabbar extends StatelessWidget {
   const Tabbar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
