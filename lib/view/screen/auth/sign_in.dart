f38d159a6377712db5c65bc5fa548726c1ea6d38
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-02-25 21:43:36 -0800

    first commit

--- lib/view/screen/auth/sign_in.dart
+++ lib/view/screen/auth/sign_in.dart
@@ -18,13 +18,7 @@ class SignIn extends StatelessWidget {
             EmailTextField(),
             SizedBox(height: 10),
             PasswordTextField(),
-            SizedBox(height: 10),
-            InkWell(
-              child: SizedBox(
-                height: 40,
-                child: Text("Sign In"),
-              ),
-            )
+            SizedBox(height: 20),
           ],
         ),
       ),
