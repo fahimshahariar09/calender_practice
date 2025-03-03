0b2f9aee3871204567aec5e9db5120e62ee38950
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-03-02 23:11:09 -0800

    first commit

--- lib/view/screen/design.dart
+++ lib/view/screen/design.dart
@@ -50,6 +50,11 @@ class Design extends StatelessWidget {
             width: 50,
             decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
             child: Text("data"),
+          ),Container(
+            height: 20,
+            width: 50,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+            child: Text("data"),
           ),
 
         ],
